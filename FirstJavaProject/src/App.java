5ddecdb8b167beaf1e5d94cf5e2ef28560105506
Kishowth Moganasundaram <46637611+kishowth@users.noreply.github.com>
2021-01-22 20:40:03 -0500

    First Upload
    Yes

---
+++ FirstJavaProject/src/App.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class App {
+
+    //variables 
+
+    //frame
+    private static JFrame openingFrame = new JFrame();
+
+    //panel
+    private static JPanel openingPanel = new JPanel();
+
+    //labels
+    private static JLabel loginLabel = new JLabel();
+    private static JLabel passwordLabel = new JLabel();
+    
+
+    //Login info
+
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
